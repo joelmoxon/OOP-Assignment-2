e335977783d052dcb9ef8315bc9148c4cafc7a1b
joelmoxon <moxonjoeljames@gmail.com>
2025-10-31 12:59:08 +0000

    Amend file structure

---
+++ Aircraft Lighting System/Aircraft Lighting System/Backend (Sam add items)/Backend_Placeholder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aircraft_Lighting_System.Backend
+{
+    internal class Backend_Placeholder
+    {
+    }
+}
---
+++ Aircraft Lighting System/Aircraft Lighting System/Lights (Joel add items)/Lights_Placeholder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aircraft_Lighting_System.Lights__Joel_add_items_
+{
+    internal class Lights_Placeholder
+    {
+    }
+}
